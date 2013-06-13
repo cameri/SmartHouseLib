a25594db744833c5ce49ba8c786e5e4961f0416f
Cameri <ricardo.arturo.cabral@gmail.com>
2013-06-13 13:13:11 -0400

    Fixed typo in InvalidControllerState.

--- src/smarthouselib/exceptions/InvalidControllerState.java
+++ src/smarthouselib/exceptions/InvalidControllerState.java
@@ -4,7 +4,7 @@ package smarthouselib.exceptions;
  * @author cameri
  * @since 6/13/13
  */
-public class InvalidControlleState extends Exception
+public class InvalidControllerState extends Exception
 {
 
 }
